fadc4e3bac03737d158d7bcd6bfc02e944feb7c1
Tarik Čelik <tcelik@devlogic.eu>
2015-01-29 20:30:29 +0100

    Added Adnan Kukuljac as @author && Reformated the code

--- didalog.js
+++ didalog.js
@@ -1,6 +1,7 @@
 /**
  * @desc This lib (DIDALOG) is a wrapper for the browser's native debug logger
  * @author Tarik Čelik tarik.chelik@gmail.com
+ * @author Adnan Kukuljac adnankukuljac@gmail.com
  * @version 0.0 (Still under heavy development and is not expected to work)
  * @license Apache 2.0 http://www.apache.org/licenses/LICENSE-2.0
  * @usage
@@ -13,11 +14,10 @@ DIDALOG = {
     isDebug: true,
     defaultMessage: 'This is the default log message. Please enter your log message.',
     callbackFunction: '',
-    logConsoleLevel: ['info', 'error', 'warn', 'debug'],
     datetime: true,
     defaultGroup: 'DIDALOG LOG',
 
-/**
+    /**
      * @desc This method does the actual logging
      * @param level
      * @param msg
@@ -26,9 +26,9 @@ DIDALOG = {
 
         level = this.checkLevel(level) ? level : this.defaultLogLevel;
         msg = msg || this.defaultMessage;
-        
+
         return this.checkConsoleSupport() == true ? this.translateLogLevel(level, this.logMsg(level, msg))
-                                                : alert("Console not supported");
+            : alert("Console not supported");
 
     },
 
@@ -37,7 +37,7 @@ DIDALOG = {
      * @returns {boolean}
      */
     checkConsoleSupport: function () {
-        return (window.console) ? true : false ;
+        return (window.console) ? true : false;
     },
 
     /**
@@ -56,79 +56,79 @@ DIDALOG = {
     },
 
     /**
-        This method writes msg on standard output (console)
-    */
-    logMsg : function(level, msg){
+     This method writes msg on standard output (console)
+     */
+    logMsg: function (level, msg) {
 
 
-        if(this.datetime){
-            var currentDate = new Date(); 
+        if (this.datetime) {
+            var currentDate = new Date();
             var printDatetime = "Log time: " + currentDate.getDate() + "/"
-                            + (currentDate.getMonth()+1)  + "/" 
-                            + currentDate.getFullYear() + " @ "  
-                            + currentDate.getHours() + ":"  
-                            + currentDate.getMinutes() + ":" 
-                            + currentDate.getSeconds();
+                + (currentDate.getMonth() + 1) + "/"
+                + currentDate.getFullYear() + " @ "
+                + currentDate.getHours() + ":"
+                + currentDate.getMinutes() + ":"
+                + currentDate.getSeconds();
             return (level + ": " + msg + " " + printDatetime);
         }
-        else 
+        else
             return (level + ": " + msg);
-    },  
+    },
 
-    translateLogLevel: function(level, msg){
+    translateLogLevel: function (level, msg) {
 
-        if(this.lvltoNumber(level) == 0)
+        if (this.lvltoNumber(level) == 0)
             return console.info(msg);
-        else if(this.lvltoNumber(level) == 1)
+        else if (this.lvltoNumber(level) == 1)
             return console.error(msg);
-         else if(this.lvltoNumber(level) == 2)
+        else if (this.lvltoNumber(level) == 2)
             return console.warn(msg);
-         else if(this.lvltoNumber(level) == 3)
+        else if (this.lvltoNumber(level) == 3)
             return console.debug(msg);
     },
 
     /**
-        This method returns index of specified level
-    */
-    lvltoNumber: function(level){
+     This method returns index of specified level
+     */
+    lvltoNumber: function (level) {
 
         return this.logLevels.indexOf(level);
     },
 
     /**
-        This method checks does level exists
-    */
-    checkLevel: function(level){
+     This method checks does level exists
+     */
+    checkLevel: function (level) {
 
         return (Array.apply(null, this.logLevels).indexOf(level) != -1)
 
     },
 
     /**
-        This method will clear console output
-    */
-    clear: function(){
-         return this.checkConsoleSupport() == true ? console.clear()
-                                                : alert("Console not supported");
-    }, 
+     This method will clear console output
+     */
+    clear: function () {
+        return this.checkConsoleSupport() == true ? console.clear()
+            : alert("Console not supported");
+    },
 
     /**
-        This method will start console group
-    */
-    groupLogStart: function(groupName){
+     This method will start console group
+     */
+    groupLogStart: function (groupName) {
         msg = groupName || this.defaultGroup;
 
         return this.checkConsoleSupport() == true ? console.group("Logging '%s'", msg)
-                                                : alert("Console not supported");  
-    }, 
+            : alert("Console not supported");
+    },
 
     /**
-        This method will end console group
-    */
-    groupLogEnd: function(){
+     This method will end console group
+     */
+    groupLogEnd: function () {
 
-        return this.checkConsoleSupport() == true ?  console.groupEnd()
-                                                : alert("Console not supported");  
+        return this.checkConsoleSupport() == true ? console.groupEnd()
+            : alert("Console not supported");
     }
 
 
